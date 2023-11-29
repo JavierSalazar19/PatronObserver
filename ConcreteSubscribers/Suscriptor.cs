948551df309b5440964622f3c743c0801ba956f9
JavierSalazar19 <javierasc2002@gmail.com>
2023-11-29 08:31:46 -0700

    Agregar archivos de proyecto.

---
+++ ConcreteSubscribers/Suscriptor.cs
@@ -0,0 +1,24 @@
+﻿using PatronObserver.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatronObserver.ConcreteSubscribers
+{
+    public class Suscriptor : ISuscriptor
+    {
+        private string _nombre;
+
+        public Suscriptor(string nombre) 
+        {
+            _nombre = nombre;
+        }
+
+        public void Update(string nuevoVideo)
+        {
+            Console.WriteLine($"[{_nombre}] voy a a ver el video titulado: {nuevoVideo}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using PatronObserver.ConcreteSubscribers;
+using PatronObserver.Interfaces;
+using PatronObserver.Publisher;
+
+Canal canal = new Canal();
+
+ISuscriptor s1 = new Suscriptor("Sergio");
+ISuscriptor s2 = new Suscriptor("Messi");
+ISuscriptor s3 = new Suscriptor("Ronaldo");
+
+canal.Suscribir(s1);
+canal.Suscribir(s2);
+canal.Suscribir(s3);
+canal.PublicarVideo("Planeta Vegetta");
+canal.PublicarVideo("Mas chorizo");
